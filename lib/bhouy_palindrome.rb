dc2f470f2382f64def90ad5017f01632da8582e5
Benjamin Houy <hello@benjaminhouy.com>
2021-03-16 14:14:39 +0000

    Finish working and refactored palindrome method

--- lib/bhouy_palindrome.rb
+++ lib/bhouy_palindrome.rb
@@ -1,8 +1,25 @@
-# frozen_string_literal: true
-
 require_relative "bhouy_palindrome/version"
 
-module BhouyPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-zA-Z]/i).join.downcase
+    end
 end
+
+
+
+
+
+
+
+
+
